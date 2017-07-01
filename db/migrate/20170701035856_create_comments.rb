e7efd53d799dcc6541e8b3dc7acef75959046ca6
sagae keiga <sagae5.28rujeae@gmail.com>
2017-07-01 06:27:37 +0000

    f

--- db/migrate/20170701035856_create_comments.rb
+++ db/migrate/20170701035856_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :name, null: false, default: "名無っしー"
+      t.text :name
       t.text :content
       t.text :product_id
 
